7c0978a77237cf56c8441a1f8968d7169e351c92
Emanuel Metaxotos <sharingan9119@hotmail.com>
2016-07-30 14:34:30 +1000

    relerbj

--- Assets/MaouOverlord_Script.cs
+++ Assets/MaouOverlord_Script.cs
@@ -4,9 +4,35 @@ using System.Collections;
 
 public class MaouOverlord_Script : MonoBehaviour {
 
-	public Text people;
+	public Text People;
 	public Text AP;
 	public Text Nuke;
+	public Text Frames;
 
+	void Start()
+	{
+		string NukeTxt = "/10";
+
+		int PeopleNum = 0;
+		int APNum = 0;
+		int NukeNum = 0;
+
+
+
+
+		Nuke.text = NukeNum.ToString() + "/10";
+		AP.text = APNum.ToString() + "/20";
+		People.text = PeopleNum.ToString ()+ "/10";
+	}
+
+	void Update()
+	{
+
+		int totalFrames = Time.frameCount;
+
+		Frames.text = totalFrames.ToString ();
+
+		
+	}
 
 }
