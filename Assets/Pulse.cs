6900768cea9ebd0f5364c441c288ca52685b325f
Emanuel Metaxotos <sharingan9119@hotmail.com>
2016-07-30 16:47:14 +1000

    Other stuff

---
+++ Assets/Pulse.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Pulse : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject myObject;
+	float i = 0f; 
+	// Update is called once per frame
+	void Update () {
+
+		float hole = Mathf.Sin (Time.timeSinceLevelLoad * 13f) * 3f + i; //freq , amp , i
+			i += 0.1f;
+		myObject.transform.localScale = new Vector3 (hole, hole, hole); 
+	}
+}
